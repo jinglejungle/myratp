cae3c36b5acdc0dee4338c7aff3fe1da24568b2e
hodd <hodd@gmx.com>
2017-06-21 11:22:28 +0200

    trafic component, styles css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LigneComponent } from './ligne/ligne.component';
-import { ItineraireComponent } from './itineraire/itineraire.component'
+import { ItineraireComponent } from './itineraire/itineraire.component';
+import { TraficComponent } from './ligne/trafic/trafic.component';
 
 const routes: Routes = [
-  { path: 'itineraire/:type/:line',   component:ItineraireComponent  },
-  { path: 'horaires/:type/:line',        component:LigneComponent  },
-  { path: '',   redirectTo: '/horaires', pathMatch: 'full' },
+  { path: 'itineraire/:type/:line', component:ItineraireComponent  },
+  { path: 'trafic', component:TraficComponent  },
+  { path: 'horaires/:type/:line', component:LigneComponent  },
+  { path: '', redirectTo: '/trafic', pathMatch: 'full' }
 ];
 
 @NgModule({
